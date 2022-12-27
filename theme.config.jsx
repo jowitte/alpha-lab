a6db2673db2e992b4d884a1e66b7773edfcbd70c
Jochen Witte <jo.witte@alpha-lab.net>
2022-12-27 22:05:54 +0100

    commit: 2022-12-27 22:05:54

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,8 +1,15 @@
 export default {
- logo: <span>Jochen Witte</span>,
- project: {
-     link: 'https://www.alpha-lab.net',
- },
- // ...
+    logo: <span>alpha-lab.net</span>,
+    docsRepositoryBase: 'https://github.com/jowitte/alpha-lab/tree/master/pages',
+    project: {
+        link: 'https://github.com/jowitte/alpha-lab/tree/main/pages',
+
+    },
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s | alpha-lab.net'
+        }
+    },
+    // ...
 }
 
